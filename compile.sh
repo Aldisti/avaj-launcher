b85f7aba2b238ef6fb953eb36a8df83300ae0cd2
aldisti <alessandrodiste99@gmail.com>
2025-06-14 17:38:53 +0200

    created compile script

---
+++ compile.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+RESET="\033[0m"
+BLACK="\033[1;30m"
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+PURPLE="\033[1;35m"
+CYAN="\033[1;36m"
+WHITE="\033[1;37m"
+
+
+# 1: type of log
+# 2: log message
+# 3: color
+_log() { echo -e "$3$1${RESET}: $2"; }
+info() { _log "INFO" "$1" "${GREEN}"; }
+warning() { _log "WARNING" "$1" "${YELLOW}"; }
+error() { _log "ERROR" "$1" "${RED}"; }
+
+set -e
+
+# no args
+compile() {
+    local files="$(find * -name "*.java")"
+    if [ "${files}" = "" ]; then
+        warning "No file to compile found!"
+        return 1
+    fi
+    javac ${files}
+}
+
+# no args
+clean() {
+    local compiled_files="$(find * -name "*.class" | tr '\n' ' ')"
+    if ! [ "${compiled_files}" = "" ]; then
+        rm ${compiled_files}
+        info "Files removed"
+    fi
+}
+
+
+while getopts ":cC" opt; do
+    case ${opt} in
+        c)
+            compile
+            ;;
+        C)
+            clean
+            ;;
+        ?)
+            error "Invalid option: ${opt}"
+            exit 1
+        ;;
+    esac
+done
